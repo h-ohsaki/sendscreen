ac09b3077ca704faeda1c12daf0348fdbf39df2a
Hiroyuki Ohsaki <code@lsnl.jp>
2019-07-09 14:05:24 +0900

    version 1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='sendscreen',
-    version='1.0',
+    version='1.2',
     author='Hiroyuki Ohsaki',
     author_email='ohsaki@lsnl.jp',
     description='Display local desktop on remove host over LAN without VGA/HDMI cable',
